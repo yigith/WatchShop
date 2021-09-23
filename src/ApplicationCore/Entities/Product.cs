ca633930bf289e473796ef6fffd3218a8c3c9822
Yiğit Hacıefendioğlu <yigith1@gmail.com>
2021-09-23 10:55:55 +0300

    entities

---
+++ src/ApplicationCore/Entities/Product.cs
@@ -0,0 +1,19 @@
+﻿namespace ApplicationCore.Entities
+{
+    public class Product : BaseEntity
+    {
+        public string ProductName { get; set; }
+
+        public string Description { get; set; }
+
+        public decimal Price { get; set; }
+
+        public string PictureUri { get; set; }
+
+        public int CategoryId { get; set; }
+        public Category Category { get; set; }
+
+        public int BrandId { get; set; }
+        public Brand Brand { get; set; }
+    }
+}
